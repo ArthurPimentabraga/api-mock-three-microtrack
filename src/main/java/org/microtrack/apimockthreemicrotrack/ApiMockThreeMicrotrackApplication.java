a96eda79c0bf49a7dd47c3482591ea3a2d6bd65a
Arthur <pimentabraga2@gmail.com>
2023-10-03 19:42:28 -0300

    Initial commit

---
+++ src/main/java/org/microtrack/apimockthreemicrotrack/ApiMockThreeMicrotrackApplication.java
@@ -0,0 +1,13 @@
+package org.microtrack.apimockthreemicrotrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMockThreeMicrotrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMockThreeMicrotrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/microtrack/apimockthreemicrotrack/ApiMockThreeMicrotrackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.microtrack.apimockthreemicrotrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMockThreeMicrotrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
